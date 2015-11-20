81f3f45b730520f66d035093bbcbd04eb9614c61
JayThaGreat <maskdbandit@gmail.com>
2015-11-20 16:44:33 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,31 @@
+class Dog
+  def bark
+    puts "Woof!"
+  end
+
+  def sit
+    puts "The Dog is sitting"
+  end
+
+  def initialize(new_name,new_breed= "Mut")
+    @name = new_name
+    @breed = new_breed
+  end
+
+  def name=(new_name)
+    @name = new_name
+  end
+
+  def name
+    @name
+  end
+
+  def breed=(new_breed)
+    @breed = new_breed
+  end
+
+   def breed
+    @breed
+   end
+end
+
